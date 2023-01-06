96f9ff39682d97da9231f1d6e2e110c6d00841f0
Mohsin Bepari <47427530+moshinnb@users.noreply.github.com>
2023-01-06 17:14:39 +0530

    Add files via upload

---
+++ C-Sharp-Assignment/Assignment07-PrimeNo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SampleConsleApp
+{
+    class primenumber
+    {
+        public static bool isPrimeNumber(int num)
+        {
+            if (num <= 1)
+            {
+                return false;
+            }
+            else if (num == 2 || num == 3 || num == 5)
+                return true;
+
+            else if (num % 2 == 0 || num % 3 == 0 || num % 5 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 5; i <= Math.Sqrt(num); i += 5)
+                {
+                    if (num % i == 0 || num % (i + 2) == 0)
+                    { return false; }
+                }
+                return true;
+            }
+        }
+    }
+    class Assignment07_PrimeNo
+    {
+        static void Main(string[] args)
+        {
+           
+            do
+            {
+                Console.WriteLine("enter the number to check prime number");
+                int num;
+                if(!int.TryParse(Console.ReadLine(), out num))
+                    break;
+
+                string res = primenumber.isPrimeNumber(num) ? "Prime Number" : "Not a Prime Number";
+                Console.WriteLine($"{num} is {res}");
+                Console.WriteLine("Press Enter to continue");
+                Console.ReadLine();
+                Console.Clear();
+            } while (true);
+        }
+    }
+}
+
+
+
+
+
+
