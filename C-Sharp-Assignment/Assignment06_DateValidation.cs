3383002e1ff65dfd914a2383cc55a6e020d68be7
Mohsin Bepari <47427530+moshinnb@users.noreply.github.com>
2023-01-06 17:14:17 +0530

    Add files via upload

---
+++ C-Sharp-Assignment/Assignment06_DateValidation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsleApp
+{
+    
+
+    class Assignment06_DateValidation
+    {
+        static bool isValidDate(int year, int month, int day)
+        {
+            try {
+                new DateTime(year, month, day);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+           
+           
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2018,13,1--->   " + isValidDate(2018,13,1));
+            Console.WriteLine("2018, 2, 29-->  " + isValidDate(2018, 2, 29));
+            Console.WriteLine("2016, 2, 29-->  " + isValidDate(2016, 2, 29));
+            
+        }
+    }
+}
