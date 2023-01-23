80a5611d6d6eecd6abab6c192c718d92e7faa7b3
Mohsin Bepari <47427530+moshinnb@users.noreply.github.com>
2023-01-23 17:59:14 +0530

    Add files via upload

---
+++ C-Sharp-Assignment/Assignment-Association_Program-BillGeneration/Item.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFramework.BillGeneration
+{
+    //Create a Class by name Bill
+ //Bill has properties: BillNo, BillDate, BillHolder, BillAmount
+ //Create a class called Item: Id, Perticulars, UnitPrice, Quantity
+ //Create a Program the provides Itemized billing for a given Customer.
+    class Item
+    {
+        public int ItemId { get; set; }
+        public string ItemPerticular { get; set; }
+        public double ItemUnitPrice { get; set; }
+        public int ItemQuantity { get; set; }
+    }
+    class Bill
+    {
+        private static int id = 0;
+        public List<Item> BillItemList { get; set; }
+        
+        public int BillNo { get; set; }
+        public DateTime BillDate { get; set; } = new DateTime();
+
+        public string BillHolder { get; set; }
+        public double BillAmount { get; private set; }
+        public Bill(){
+            id++;
+            BillNo = id;
+            }
+        public void AddBillAmount(double amount)
+        {
+            BillAmount += amount;
+        }
+
+    }
+    class Shopmanager
+    {
+        static List<Item> p  = new List<Item>();
+        static List<Bill> BillList = new List<Bill>();
+        //public HashSet<Item> ItemList { get; set; }
+        public HashSet<Item> ItemList = new HashSet<Item>();
+        public Shopmanager()
+        {
+            ItemList.Add(new Item { ItemId=1,ItemPerticular = "Bottle", ItemUnitPrice = 100 });
+            ItemList.Add(new Item { ItemId = 2, ItemPerticular = "Pen", ItemUnitPrice = 80 });
+            ItemList.Add(new Item {  ItemId=3,ItemPerticular = "Book", ItemUnitPrice = 60 });
+            ItemList.Add(new Item {  ItemId=4,ItemPerticular = "Bag", ItemUnitPrice = 200 });
+            ItemList.Add(new Item { ItemId=5,ItemPerticular = "Pencil", ItemUnitPrice = 12 });
+
+        }
+        public  Item findItem(int id)
+        {
+            foreach (var item in ItemList)
+            {
+                if (item.ItemId == id)
+                    return item;
+            }
+            return null;
+        }
+        public void displayItems()
+        {
+            foreach (var item in ItemList)
+            {
+                Console.WriteLine($"{item.ItemPerticular.ToUpper()} ---{item.ItemUnitPrice}/- :------ ItemId:{item.ItemId}");
+            }
+        }
+        public Bill newBill = new Bill();
+        
+    }
+}
---
+++ C-Sharp-Assignment/Assignment-Association_Program-BillGeneration/MainBillApp.cs
@@ -0,0 +1,86 @@
+﻿using SampleConsleApp.BillGeneration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFramework.BillGeneration
+{
+    class MainBillApp
+    {
+        static int ItemManager(Shopmanager BillSystem)
+        {
+            var item = BillSystem.newBill;
+            if (item.BillItemList == null) item.BillItemList = new List<Item>();
+            //if (item == null) item = new List<Item>();
+            BillSystem.displayItems();
+            int id = Utilities.GetNumber("Enter Item Id \\ TO Generate Bill enter 0");
+            if (id == 0) return 0;
+            int Qnt = Utilities.GetNumber("Enter Qantity");
+          
+            if (item.BillItemList.Exists(e => e.ItemId == id))
+            {
+                item.BillItemList.Find(e => e.ItemId == id).ItemQuantity +=
+                    Qnt;
+            }
+            else
+            {
+                var fItem = BillSystem.findItem(id);//    .Find((e) => e.ItemId == id);
+                //if (item.BillItemList != null)
+                    fItem.ItemQuantity = Qnt;
+
+                item.BillItemList.Add(fItem);
+            }
+            return id;
+            
+
+        }
+        static void Billmanager()
+        {
+            Shopmanager BillSystem = new Shopmanager();
+            string CName = Utilities.Prompt("Enter Custmore Name"); 
+            BillSystem.newBill.BillHolder = CName;
+            while (Convert.ToBoolean(ItemManager(BillSystem)));
+            GenerateBill(BillSystem.newBill);
+
+
+
+        }
+
+        private static void GenerateBill(Bill newBill)
+        {
+            Console.Clear();
+            var newList = newBill.BillItemList;
+            Console.WriteLine($"Bill Holder Name:{newBill.BillHolder}                                 Date: {newBill.BillDate}\nBill No: {newBill.BillNo}");
+            double amount = 0;
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("Perticula        Quantity        Rate        Total");
+            Console.WriteLine("-------------------------------------------------------");
+            foreach (var i in newList)
+            {
+                if (i.ItemQuantity != 0)
+                {
+                    var price = i.ItemQuantity * i.ItemUnitPrice;
+                    Console.WriteLine($"{i.ItemPerticular}        {i.ItemQuantity}        {i.ItemUnitPrice}        {price}");
+                    amount += price;
+                }
+            }
+            Console.WriteLine("-------------------------------------------------------");
+            Utilities.Prompt($"Total Bill                           {amount}");
+            newBill.AddBillAmount(amount);
+            
+        }
+
+        public const string menu = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~BILL MANAGMENT SOFTWARE~~~~~~~~~~~~~~~~~~~\nTO ADD NEW ITEM------------------------>PRESS 1\nTO GENERATE BILL---------------->PRESS 2\nTO DELETE Customer------------------------->PRESS 3\nTO Display Customer ----------------->PRESS 4\nPS: ANY OTHER KEY IS CONSIDERED AS EXIT.....................................";
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Billmanager();
+            }
+           
+        }
+    }
+}
